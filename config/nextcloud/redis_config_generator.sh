8312c8d1858c25627da98adf43a4f31606e55728
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2025-03-22 11:51:23 +0100

    fix: sh hook nextcloud

--- config/nextcloud/redis_config_generator.sh
+++ config/nextcloud/redis_config_generator.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-cat <<EOF
+mkdir -p /usr/local/etc/php/conf.d/
+
+cat <<EOF > /usr/local/etc/php/conf.d/redis-session.ini
 session.save_handler = redis
 session.save_path = "tcp://${HOOK_REDIS_HOST}:6379?auth=${HOOK_REDIS_HOST_PASSWORD}"
 redis.session.locking_enabled = 1
 redis.session.lock_retries = -1
 redis.session.lock_wait_time = 10000
-EOF > /usr/local/etc/php/conf.d/redis-session.ini
+EOF
