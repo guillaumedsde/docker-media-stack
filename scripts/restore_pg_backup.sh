d8f0fc115aa9162988fc06833c009962629498cd
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2025-05-29 11:58:53 +0200

    fix: no TTY

--- scripts/restore_pg_backup.sh
+++ scripts/restore_pg_backup.sh
@@ -10,4 +10,4 @@ DOCKER_DATA_DIR="${DOCKER_DATA_DIR}"
 DOCKER_USER="${DOCKER_USER}"
 
 docker compose up --wait --pull always "${DB_SERVICE_NAME}"
-docker compose exec "${DB_SERVICE_NAME}" /bin/sh -c 'psql --host localhost --user "${POSTGRES_USER}"' < "${SERVICE_NAME}_$(date --iso-8601).out"
+docker compose exec --no-TTY "${DB_SERVICE_NAME}" /bin/sh -c 'psql --host localhost --user "${POSTGRES_USER}"' < "${SERVICE_NAME}_$(date --iso-8601).out"
