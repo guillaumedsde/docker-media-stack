ecbdf14c823df6a57701f321e93cb0b6f4c41562
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2025-05-29 11:56:14 +0200

    fix: invalid option

--- scripts/restore_pg_backup.sh
+++ scripts/restore_pg_backup.sh
@@ -9,5 +9,5 @@ DB_SERVICE_NAME="${DB_SERVICE_NAME:-"${SERVICE_NAME}-db"}"
 DOCKER_DATA_DIR="${DOCKER_DATA_DIR}"
 DOCKER_USER="${DOCKER_USER}"
 
-docker compose up --wait --pull --always "${DB_SERVICE_NAME}"
+docker compose up --wait --pull always "${DB_SERVICE_NAME}"
 docker compose exec "${DB_SERVICE_NAME}" /bin/sh -c 'psql --host localhost --user "${POSTGRES_USER}"' < "${SERVICE_NAME}_$(date --iso-8601).out"
