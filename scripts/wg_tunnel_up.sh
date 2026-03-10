f455708c4decf255f7ba419a3ee1983de660d927
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-03-10 20:53:05 +0100

    fix(wg): restart services when tunnel is down

---
+++ scripts/wg_tunnel_up.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+vpn_interface="${1}"
+port_number="${2}"
+
+# Set the Qbittorrent prot number
+wget "http://localhost:8080/api/v2/app/setPreferences" \
+    --output-document - \
+    --no-verbose \
+    --retry-connrefused \
+    --post-data \
+    "json={\"listen_port\":${port_number},\"current_network_interface\":\"${vpn_interface}\",\"random_port\":false,\"upnp\":false}"
+
+# Restart qbittorrent
+wget "http://localhost:8080/api/v2/app/shutdown" \
+    --method POST \
+    --no-verbose \
+    --retry-connrefused
+
+PROWLARR_API_KEY=$(cat /run/secrets/PROWLARR_API_KEY | tr '\n' '')
+
+# Restart Prowlarr
+wget "http://localhost:9696/api/v1/system/restart" \
+    --header "X-Api-Key: $(PROWLARR_API_KEY)"  \
+    --method POST \
+    --no-verbose \
+    --retry-connrefused
