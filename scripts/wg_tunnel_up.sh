85128cb43f34b7d457481128ba1e36bc67beb1f0
guillaumedsde <30023543+guillaumedsde@users.noreply.github.com>
2026-05-14 14:19:44 +0200

    fix: stop restarting other services on wg tunnel up

--- scripts/wg_tunnel_up.sh
+++ scripts/wg_tunnel_up.sh
@@ -4,27 +4,10 @@ set -eu
 vpn_interface="${1}"
 port_number="${2}"
 
-# Set the Qbittorrent prot number
+# Set the Qbittorrent port number
 wget "http://localhost:8080/api/v2/app/setPreferences" \
     --output-document - \
     --no-verbose \
     --retry-connrefused \
     --post-data \
     "json={\"listen_port\":${port_number},\"current_network_interface\":\"${vpn_interface}\",\"random_port\":false,\"upnp\":false}"
-
-# Restart qbittorrent
-wget "http://localhost:8080/api/v2/app/shutdown" \
-    --method POST \
-    --output-document - \
-    --no-verbose \
-    --retry-connrefused
-
-PROWLARR_API_KEY=$(cat /run/secrets/PROWLARR_API_KEY)
-
-# Restart Prowlarr
-wget "http://localhost:9696/api/v1/system/restart" \
-    --header "X-Api-Key: ${PROWLARR_API_KEY}"  \
-    --method POST \
-    --output-document - \
-    --no-verbose \
-    --retry-connrefused
